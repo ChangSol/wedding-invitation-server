916e5d5cd21cf64586112dd85e86e5d864bce81e
ChangSol <ckdwnsla12@naver.com>
2023-08-20 20:09:01 +0900

    축하글, 축하글좋아요 엔티티 재설계

--- modules/domain-congratulation/src/main/java/org/changsol/congratulations/domains/Congratulation.java
+++ modules/domain-congratulation/src/main/java/org/changsol/congratulations/domains/Congratulation.java
@@ -1,23 +1,26 @@
 package org.changsol.congratulations.domains;
 
+import com.google.common.collect.Sets;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Congratulation {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -25,4 +28,28 @@ public class Congratulation {
 	@Lob
 	@NotNull
 	private String contents;
+
+	/**
+	 * 다른 애그리거트이므로 간접참조
+	 */
+	private Long memberId;
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<CongratulationLike> congratulationLikes = Sets.newHashSet();
+
+	@Builder
+	public Congratulation(String contents) {
+		this.contents = Objects.requireNonNull(contents, "contents is required");
+	}
+
+	public void likeUp(Long memberId) {
+		congratulationLikes.add(CongratulationLike.builder()
+												  .congratulation(this)
+												  .memberId(memberId)
+												  .build());
+	}
+
+	public void likeDown(CongratulationLike congratulationLike) {
+		congratulationLikes.remove(congratulationLike);
+	}
 }
--- modules/domain-congratulation/src/main/java/org/changsol/congratulations/domains/CongratulationLike.java
+++ modules/domain-congratulation/src/main/java/org/changsol/congratulations/domains/CongratulationLike.java
@@ -1,32 +1,46 @@
 package org.changsol.congratulations.domains;
 
+import java.util.Objects;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-@Builder
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(
+	uniqueConstraints = {
+		@UniqueConstraint(columnNames = {"congratulation_id", "member_id"}),
+	}
+)
 public class CongratulationLike {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Lob
 	@NotNull
-	private String contents;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Congratulation congratulation;
 
 	/**
 	 * 다른 애그리거트이므로 간접참조
 	 */
+	@NotNull
 	private Long memberId;
+
+	@Builder
+	public CongratulationLike(Congratulation congratulation, Long memberId) {
+		this.congratulation = Objects.requireNonNull(congratulation, "congratulation is required");
+		this.memberId = Objects.requireNonNull(memberId, "memberId is required");
+	}
 }
